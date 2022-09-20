3861ebb3a0f29ea82acfde9318859ce754f24684
FickleIlya <fickle041@gmail.com>
2022-09-20 21:45:22 +0300

    url

--- code/apps/api/urls.py
+++ code/apps/api/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('items', GetAllItems.as_view(), name='items'),
-    path('buy/<int:item_id>', BuyItem.as_view(), name='buy_item'),
-    path('item/<int:item_id>', GetItem.as_view(), name='item'),
-    path('order/<str:order_id>', OrderAPI.as_view(), name='order'),
+    path('items/', GetAllItems.as_view(), name='items'),
 
-    path('success', SuccessUrl.as_view(), name='success'),
-    path('cancel', CancelUrl.as_view(), name='cancel')
+    path('buy/<int:item_id>/', BuyItem.as_view(), name='buy_item'),
+    path('item/<int:item_id>/', GetItem.as_view(), name='item'),
+
+    path('order/<str:order_id>/', OrderAPI.as_view(), name='order'),
+    path('order/', OrderCreateAPI.as_view(), name='create_order'),
+
+    path('success/', SuccessUrl.as_view(), name='success'),
+    path('cancel/', CancelUrl.as_view(), name='cancel')
 ]
